8219c300885851aa58e6877d052aeab24d4365f1
kedi <147631009+7heWatter@users.noreply.github.com>
2025-10-21 14:45:21 -0300

    333333333333

--- main.js
+++ main.js
@@ -79,24 +79,48 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
-function mostraPergunta(){
+function mostraPergunta() {
+    if (atual)>= perguntas.length{
+        mostraResultado();
+        return;
+    }
+
+
+
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = " ";
     mostraAlternativas();
 }
 
 
-function mostraAlternativas(){
+function mostraAlternativas() {
     for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener('click',function(){
-            atual++;
-            mostraPergunta();
-        })
+        botaoAlternativas.addEventListener('click', () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
+
+
+function respostaSelecionada (opcaoSelecionada){
+    afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual ++;
+    mostraPergunta();
+}
+function mostraResultado(){
+    caixaPerguntas.textContent = "Se Fosse Possível...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = " ";
+}
+
+
+
+
+
 
 mostraPergunta();
